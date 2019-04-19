f1d2f7b20448c61c00d8df3999b9c733f891d201
MacKenzie Jew <mack.jew@gmail.com>
2019-04-19 11:15:38 -0700

    Progressing on Recipe Project. Added Ingredient Model class.

--- RecipeProject/src/app/header/recipe-list/recipe-list.component.ts
+++ RecipeProject/src/app/header/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from '../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  private recipes = [];
+  public recipes: Recipe[] = [ new Recipe('Test Recipe', 'This is a test', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'), new Recipe('Fish Recipe', 'Cook the fish', 'https://thumbs-prod.si-cdn.com/qXrJJ-l_jMrQbARjnToD0fi-Tsg=/800x600/filters:no_upscale()/https://public-media.si-cdn.com/filer/d6/93/d6939718-4e41-44a8-a8f3-d13648d2bcd0/c3npbx.jpg'), new Recipe('Chicken Recipe', 'Cook the chicken', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTFl00_xsx9ItRPlD7Ii6fG_f0cQMDOdHsosRvZs7wIrjuKLY8'), new Recipe('Vegetable Recipe', 'Cook the vegetable', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZdwBDQZeSsqNGmobwpuZL7bAFKbaRX88NjBurbbY8EL4pbpJV') ];
 
   constructor() { }
 
